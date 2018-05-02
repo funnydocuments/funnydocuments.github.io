30b8e390bd3838260e66e954b843e7237ef0b722
cuongtd <cuongtran.dc@gmail.com>
2018-05-02 22:34:35 +0700

    edit index file

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     $(".list-modules > ul > li").mouseover(function() {
         $(".description-modules .des-md").removeClass("show");
         var des_module = $(this).attr("data-description");
+        console.log(des_module);
         $("#" + des_module).addClass("show");
     });
 });

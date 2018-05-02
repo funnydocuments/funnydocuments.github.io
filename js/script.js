9ce559ae83517f749dd7c34c0def10f829790de1
cuongtd <cuongtran.dc@gmail.com>
2018-05-02 23:46:17 +0700

    edit index file

--- js/script.js
+++ js/script.js
@@ -1,19 +1,9 @@
 $(document).ready(function() {
-    function stickyBlock(){
-        $("#left-container, #left-container").stick_in_parent();
-    }
-
-    stickyBlock();
-
     $(".list-modules > ul > li").mouseover(function() {
         $(".description-modules .des-md").removeClass("show");
         var des_module = $(this).attr("data-description");
-        console.log(des_module);
         $("#" + des_module).addClass("show");
-        //stickyBlock();
     });
 
-
-
-
+    $("#left-container, #left-container").stick_in_parent();
 });
